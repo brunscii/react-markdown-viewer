3879bca6de5c296dc0f1e372e8fb2da83c28ab6f
Chris Carlin <brunscii@gmail.com>
2023-01-26 17:11:24 -0500

    Total Restucture

---
+++ src/MDbox.js
@@ -0,0 +1,75 @@
+import './App.css';
+import React, {useState} from "react";
+import DOMPurify from 'dompurify';
+import marked from 'marked';
+
+function MDPreviewer() {
+  
+  const [mdText, setmdText] = useState('')
+  const [btnText, setBtnText] = useState('Preview')
+    
+  // useEffect(()=>{
+  //   console.log(mdText)
+  // },[mdText])
+  
+  const btnChange = ()=>{
+
+    const prevBox = document.querySelector('#prev-box');
+
+    if( btnText === 'Preview' ){
+      console.log('btn pressed with prev as tite' + mdText)
+      
+      convertMDtoHTML(mdText).then( (content)=> {
+        prevBox.innerHTML = content;
+      });
+
+      document.querySelector('#md-editor').classList.add('inactive');
+      setBtnText('Edit');
+      prevBox.classList.remove('inactive');
+
+    } else {
+      
+      document.querySelector('#md-editor').classList.remove('inactive');
+      prevBox.classList.add('inactive');
+      setBtnText('Preview');
+
+    }
+  }
+
+
+  async function convertMDtoHTML(md){
+
+    if(!md){return 'Nothing to show here'}
+    
+    const options = {
+      gfm: true,
+      breaks: true
+    }
+    
+    return DOMPurify.sanitize(marked.parse( md, options ));
+  
+  }
+
+  return (
+
+    <div className="main-app">
+      
+      <button aria-label="preview" id="btn-swap" onClick={btnChange}>
+         {btnText}
+      </button>
+      
+      <textarea aria-label="Markdown Entry"
+                name="md-input" 
+                id="md-editor" cols="80" 
+                rows="30" 
+                placeholder=""
+                onInput={ (e)=> setmdText( e.target.value ) }>
+                  {mdText}
+                </textarea>
+      <div className="inactive" id="prev-box">" "</div>
+
+    </div>
+  );
+}
+
+export default MDPreviewer;
---
+++ src/Prevbox.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {useState, useEffect} from "react";
+
+
+function PreviewBox(props){
+
+    return (
+        
+        <div className="preview-box">
+
+        </div>
+
+    )
+}
